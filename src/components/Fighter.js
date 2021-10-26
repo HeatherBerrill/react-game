7c1e36feb6e26c4e2de2c6a7b70478c659ddaa55
HeatherBerrill <heather.berrill@btinternet.com>
2021-10-26 14:50:30 +0100

    creating the clickable list

--- src/components/Fighter.js
+++ src/components/Fighter.js
@@ -1,23 +1,52 @@
+import { useState } from 'react';
 import fighters from '../fighters';
-
+import '../styles/Fighter.css';
+import { AiOutlineCaretRight, AiOutlineCaretLeft } from 'react-icons/ai';
 const Fighter = () => {
+  const [fighter, setFighter] = useState(0);
+  const length = fighters.length;
+
+  const nextFighter = () => {
+    setFighter(fighter === length - 1 ? 0 : fighter + 1);
+  };
+
+  const prevFighter = () => {
+    setFighter(fighter === 0 ? length - 1 : fighter - 1);
+  };
+
+  console.log(fighter);
   return (
-    <div>
-      <ul className='fighter__list'>
-        {fighters.map((fighter) => {
-          return (
-            <li key={fighter.name}>
-              <h3> {fighter.name} </h3>
+    <section className='fighter-box'>
+      <AiOutlineCaretLeft
+        className='fighters__left-arrow'
+        onClick={prevFighter}
+      >
+        {' '}
+      </AiOutlineCaretLeft>
+      <AiOutlineCaretRight
+        className='fighters__right-arrow'
+        onClick={nextFighter}
+      >
+        {' '}
+      </AiOutlineCaretRight>
+
+      {fighters.map((singleFighter, index) => {
+        return (
+          <div
+            className={index === fighter ? 'fighter active' : 'fighter'}
+            key={index}
+          >
+            {index === fighter && (
               <img
-                alt={fighter.name}
-                className='fighter-list__thumbnail'
-                src={fighter.image}
+                alt={singleFighter.name}
+                className='fighter-image'
+                src={singleFighter.image}
               ></img>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+            )}
+          </div>
+        );
+      })}
+    </section>
   );
 };
 
